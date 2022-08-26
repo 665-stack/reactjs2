09679368addb358d9caaf9d22c9d600368d262d7
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-08-27 01:03:24 +0600

    data load by using axios and show on to create a chart

---
+++ recharts-with-axios/recharts-with-axios/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PhoneChart from './Component/PhoneChart/PhoneChart';
+
+function App() {
+  return (
+    <div className="App">
+      <PhoneChart></PhoneChart>
+    </div>
+  );
+}
+
+export default App;
