9c32410720efaec5596191694dc17945126ebb3e
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-08-27 21:23:54 +0600

    add some of components

---
+++ react-router/src/Components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>About us</h2>
+            <p>Wanna know about us?</p>
+            <p><small>Then give me some money as a lend.</small></p>
+        </div>
+    );
+};
+
+export default About;
---
+++ react-router/src/Components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h3>Hello Friends. How are you</h3>
+            <p><small>If you can, lend me some of the money.</small></p>
+        </div>
+    );
+};
+
+export default Friends;
