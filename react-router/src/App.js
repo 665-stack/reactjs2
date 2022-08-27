81ef8a2f29f6e525efa454372d67dd1a027c488c
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-08-28 01:45:23 +0600

    Create Simple Route and simple router setup

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 import Home from './Components/Home/Home';
-import Friends from './Components/Friends/Friends';
 import About from './Components/About/About';
+import Friends from './Components/Friends/Friends';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
+      <h2>Welcome to my fucking website </h2>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/friends' element={<Friends></Friends>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
